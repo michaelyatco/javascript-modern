ab2784c4eb6ccc4a4a970aaa1f45697a734a0d7d
Michael Yatco <mjyatco@gmail.com>
2017-03-15 19:18:40 -0400

    installed Babel

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
-console.log("Hello JavaScript");
 var tinycolor = require("tinycolor2");
+var moment = require("moment");
 var color = tinycolor("red");
+var datetime = moment().format('MMMM Do YYYY, h:mm:ss a');
 
+var bob = {
+  _name: "Bob",
+  _friends: [],
+  printFriends() {
+    this._friends.forEach(f =>
+      console.log(this._name + " knows " + f));
+  }
+};
+
+console.log("Hello JavaScript");
+console.log(datetime);
+console.log(bob);
